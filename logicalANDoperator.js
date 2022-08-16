5c1b93727350f9d6bbab52ea1892b5be069d7529
vinay <vinayvarma2545@gmail.com>
2022-08-16 20:35:24 +0530

    logical

---
+++ logicalANDoperator.js
@@ -0,0 +1,5 @@
+var a = 5;
+var b =10;
+(a != b) && (a<b);
+console.log(a != b) && (a<b);
+console.log(a==b) && (a<b);
---
+++ logicalORoperator.js
@@ -0,0 +1,5 @@
+a = 5;
+b = 10;
+console.log((a<b) || (a==b));
+console.log((a>b) || (a!=b));
+console.log((a>b) || (a==b));
